1ff2a1e651280f1d61407aaeced14c5991dea945
jonironman <62795102+jonironman@users.noreply.github.com>
2022-07-05 23:33:16 +0200

    modified:   blog/blog/urls.py 	modified:   blog/db.sqlite3 	modified:   blog/posts/views.py 	modified:   blog/website/templates/website/welcome.html 	modified:   blog/website/views.py 	blog/posts/forms.py 	blog/posts/templates/ 	blog/posts/urls.py 	blog/website/static/ 	blog/website/templates/base.html 	blog/website/templates/top-menu.html

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from website.views import welcome
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', welcome)
+    path('', welcome, name = 'home'),
+    path('posts/', include('posts.urls'), name = 'posts'),
+
 ]
--- blog/website/views.py
+++ blog/website/views.py
@@ -1,10 +1,18 @@
+from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
+from posts.models import Post
 
 # Create your views here.fds
 #fdf
 
 def welcome(request): 
-    return render(request, "website/welcome.html")
+    return render(request, "website/welcome.html",
+                 {
+                    "current_time": datetime.now(),
+                    "posts": Post.objects.all(),
+                    "num_posts": Post.objects.count()
+                 }
+    )
 
 
